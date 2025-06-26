a14c2d0095d6a723802eb2fcc0c317ae5033643c
anshif-8590 <anshifp949@gmail.com>
2025-06-26 13:21:04 +0530

    IMP navbar not completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
     <Layouts>
 
         <div className="absolute top-0 left-0 max-w-[1400px] w-full px-4 h-screen bg-slate-600">  
-         <div className="absolute top-[95px] left-0 w-full h-[calc(100vh-95px)]">
-            <div id="IMPnavbar" className="flex justify-center">
+         <div className="absolute top-[95px] left-0 w-full h-[calc(100vh-95px)] px-[30px]">
+            <div id="IMPnavbar" className="flex justify-center bg-red-500">
 
             <IMPNavbar></IMPNavbar>
             </div>
