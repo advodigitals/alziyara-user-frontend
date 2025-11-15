a4e9ce5d4a5347cc00c9d599cf2f6e4156bbc8b2
anshif-8590 <anshifp949@gmail.com>
2025-11-15 12:46:34 +0530

    changed the hover effect of the contact us button in the navbar

--- src/constants/ContactButton.jsx
+++ src/constants/ContactButton.jsx
@@ -7,9 +7,9 @@ const ContactButton = (props) => {
     <>
       <div id='navbarContactButton' className=" invisible lg:visible h-fit w-fit" >
         <Link to={"/contact-us"} >
-          <div className={`flex items-center justify-center rounded-[10px] bg-primary-color h-fit w-fit gap-4 py-3 px-3 border-none  hover:bg-button-hover `}>
-            <p className="text-white text-lg font-light ">CONTACT US</p>
-            <span className="text-white "><Icon icon="streamline-freehand:help-headphones-customer-support-human" width="30" height="30" className='' /></span>
+          <div className={`flex items-center justify-center rounded-[10px]  h-fit w-fit gap-4 py-3 px-3 text-white   hover:text-primary-color bg-primary-color hover:bg-white transition-all duration-[0.5s]  hover:shadow-lg hover:shadow-slate-950/40 border border-primary-color`}>
+            <p className=" text-lg fon font-light uppercase">Contact us</p>
+            <span className=" "><Icon icon="streamline-freehand:help-headphones-customer-support-human" width="30" height="30" className='' /></span>
           </div>
         </Link>
       </div>
@@ -18,3 +18,4 @@ const ContactButton = (props) => {
 }
 
 export default ContactButton
+// border-none  hover:bg-button-hover
