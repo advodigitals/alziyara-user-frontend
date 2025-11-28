bbe02ccdb838a4e15ce672154dbeacd57aa702e9
anshif-8590 <anshifp949@gmail.com>
2025-11-28 13:58:14 +0530

    removed the unwanted navigations from the service section and added the array of options to a js file in constants

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,48 +1,12 @@
 import { Icon } from '@iconify/react/dist/iconify.js'
 import React from 'react'
 import { Link } from 'react-router'
+import { ServiceCardDetails } from '../constants/ServiceSectionOptions'
 
 
 const Services = () => {
 
-  const ServiceCardDetails = [
-    {
-      icon: "mdi:flight",
-      title: "Ticketing",
-      content: "All International and Domestic ticketing service with fast and customer friendly staff supports.",
-      path: "/flights"
-    },
-    {
-      icon: "la:kaaba",
-      title: "Hajj & umrah",
-      content: "Hajj and Umrah Serivice providers with a big year of experience on the best value to money.",
-      path: "/hajj-umrah"
-    },
-    {
-      icon: "fontisto:holiday-village",
-      title: "Tour packages",
-      content: "International and Domestic budget tour packeges with best budget and proper guidence.",
-
-    },
-    {
-      icon: "guidance:passports",
-      title: "Visa Services",
-      content: "Complete visa assistance with fast and customer-friendly support for all your travel needs.",
-
-    },
-    {
-      icon: "healthicons:i-certificate-paper-outline",
-      title: "Certificate ATT",
-      content: "Trusted certificate attestation services handled quickly with care and reliable guidance.",
-      path: "/certificate-attestation"
-    },
-    {
-      icon: "fluent-mdl2:join-online-meeting",
-      title: "Online Services",
-      content: "All-in-one online services for visa processing, GCC medicals, and comprehensive travel insurance.",
-      path: "/online-services"
-    },
-  ]
+  
 
 
   return (
@@ -78,39 +42,3 @@ const Services = () => {
 export default Services
 
 
-// {
-//       icon: <Icon icon="mdi:flight" width="41" height="41" />,
-//       title: "Ticketing",
-//       content: "All International and Domestic ticketing service with fast and customer friendly staff supports.",
-//       path: "/flights"
-//     },
-//     {
-//       icon: <Icon icon="la:kaaba" width="45" height="45" />,
-//       title: "Hajj & umrah",
-//       content: "Hajj and Umrah Serivice providers with a big year of experience on the best value to money.",
-//       path: "/hajj-umrah"
-//     },
-//     {
-//       icon: <Icon icon="fontisto:holiday-village" width="41" height="41" />,
-//       title: "Tour packages",
-//       content: "International and Domestic budget tour packeges with best budget and proper guidence.",
-
-//     },
-//     {
-//       icon: <Icon icon="guidance:passports" width="41" height="41" />,
-//       title: "Visa Services",
-//       content: "Complete visa assistance with fast and customer-friendly support for all your travel needs.",
-
-//     },
-//     {
-//       icon: <Icon icon="healthicons:i-certificate-paper-outline" width="43" height="43" />,
-//       title: "Certificate ATT",
-//       content: "Trusted certificate attestation services handled quickly with care and reliable guidance.",
-//       path: "/certificate-attestation"
-//     },
-//     {
-//       icon: <Icon icon="fluent-mdl2:join-online-meeting" width="40" height="40" />,
-//       title: "Online Services",
-//       content: "All-in-one online services for visa processing, GCC medicals, and comprehensive travel insurance.",
-//       path: "/online-services"
-//     },
---
+++ src/constants/ServiceSectionOptions.js
@@ -0,0 +1,38 @@
+export const ServiceCardDetails = [
+    {
+      icon: "mdi:flight",
+      title: "Ticketing",
+      content: "All International and Domestic ticketing service with fast and customer friendly staff supports.",
+      
+    },
+    {
+      icon: "la:kaaba",
+      title: "Hajj & umrah",
+      content: "Hajj and Umrah Serivice providers with a big year of experience on the best value to money.",
+      path: "/hajj-umrah"
+    },
+    {
+      icon: "fontisto:holiday-village",
+      title: "Tour packages",
+      content: "International and Domestic budget tour packeges with best budget and proper guidence.",
+
+    },
+    {
+      icon: "guidance:passports",
+      title: "Visa Services",
+      content: "Complete visa assistance with fast and customer-friendly support for all your travel needs.",
+
+    },
+    {
+      icon: "healthicons:i-certificate-paper-outline",
+      title: "Certificate ATT",
+      content: "Trusted certificate attestation services handled quickly with care and reliable guidance.",
+      
+    },
+    {
+      icon: "fluent-mdl2:join-online-meeting",
+      title: "Online Services",
+      content: "All-in-one online services for visa processing, GCC medicals, and comprehensive travel insurance.",
+      
+    },
+  ]
